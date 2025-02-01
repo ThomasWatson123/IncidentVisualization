593682d492980ba57f6f0b8b9cbdee2b496c5505
tew5289 <thomaswatson1021@gmail.com>
2025-02-01 13:50:48 -0500

    Added modal popup when the incident is clicked on that shows all of the data for the incident

--- src/components/map-marker.tsx
+++ src/components/map-marker.tsx
@@ -1,23 +1,108 @@
-import { useEffect } from "react"
+
 import "./map-marker-styles.css"
 import { FaInfoCircle } from "react-icons/fa"
+import { useState } from "react"
+import { IncidentJson } from "../interfaces"
+import { Card, CardContent, Modal, Popper } from "@mui/material"
+import { DataGrid, GridColDef } from "@mui/x-data-grid"
 
 interface MapMarkerProps {
     lat: number,
-    lng: number
+    lng: number,
+    incident: IncidentJson
 }
 
-const MapMarker = ({lat, lng}: MapMarkerProps) => {
+const MapMarker = ({lat, lng, incident}: MapMarkerProps) => {
+    const [isPopupVisible, setPopupVisible] = useState<boolean>(false);
+    // const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+    const onMarkerClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+        // setAnchorEl(event.currentTarget)
+        setPopupVisible(true);
+    }
 
-    // const onMarkerClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const onModalClose = () => setPopupVisible(false);
 
-    // }
+    const getDateFormat = (val: string) => {
+        const date = new Date(val);
+        return date.toLocaleString();
+    }
 
     return (
-        
-        <div className="marker-icon" >
+        <>
+        <div className="marker-icon" onClick={onMarkerClick} >
             <FaInfoCircle style={{width: "100%", height: "100%"}}/>
         </div>
+        <Modal open={isPopupVisible} onClose={onModalClose} sx={{width: "50%", alignSelf: "center", justifySelf: "center", maxHeight: "90vh"}}>
+                {/* <Box sx={{ border: 1, p: 1, bgcolor: "background.paper", width: "70%", alignSelf: "center"}}> */}
+                    <div className="card">
+                        <div className="card-header">
+                            <div className="custom-vstack">
+                            <p style={{fontSize: "1.5rem", marginBottom: 0}}>Common Place Name: {incident.address.common_place_name}</p>
+                            <p style={{fontSize: "1rem", marginBottom: 0}}>Address: {incident.address.address_line1}</p>
+                            </div>
+                        </div>
+                        <div className="card-body" style={{ height: "70vh", overflow: "auto"}}>
+                            <h5>Fire Department</h5>
+                            <p className="data-p"><span className="label">Name</span>: {incident.fire_department.name}</p>
+                            <div className="custom-hstack">
+                                <p className="data-p me-3"><span className="label">Shift</span>: {incident.fire_department.shift}</p>
+                                <p className="data-p me-3"><span className="label">State</span>: {incident.fire_department.state}</p>
+                                <p className="data-p me-3"><span className="label">Time Zone</span>: {incident.fire_department.timezone}</p>
+                            </div>
+                            <hr />
+                            <h5>Description</h5>
+                            <p className="data-p"><span className="label">Comments</span>: {incident.description.comments}</p>
+                            <div className="custom-hstack">
+                                <p className="data-p me-5"><span className="label">Event Opened</span>: {getDateFormat(incident.description.event_opened)}</p>
+                                <p className="data-p"><span className="label">Event Closed</span>: {getDateFormat(incident.description.event_closed)}</p>
+                            </div>
+                            
+                            <div className="custom-hstack">
+                            <p className="data-p me-1"><span className="label">First Unit Dispatched</span>: {getDateFormat(incident.description.first_unit_dispatched)}</p>
+                            <p className="data-p me-1"><span className="label">First Unit Enroute</span>: {getDateFormat(incident.description.first_unit_enroute)}</p>
+                            <p className="data-p"><span className="label">First Unit Arrived</span>: {getDateFormat(incident.description.first_unit_arrived)}</p>
+                            </div>
+                            <p className="data-p"><span className="label">LOI Search Complete</span>: {getDateFormat(incident.description.loi_search_complete)}</p>
+                            <div className="custom-hstack">
+                                <p className="data-p me-5"><span className="label">Type</span>: {incident.description.type}</p>
+                                <p className="data-p"><span className="label">Sub Type</span>: {incident.description.subtype}</p>
+                            </div>
+                            
+                            <hr />
+                            <h5>Apparatuses</h5>
+                            <div className="wrappable-custom-hstack">
+                                {incident.apparatus.map((item, i) => {
+                                    return (
+                                        <Card key={item.car_id} style={{ width: "50%"}}>
+                                            <CardContent>
+                                                <p className="data-p"><span className="label">Unit ID</span>: {item.unit_id}</p>
+                                                <hr />
+                                                <p className="data-p"><span className="label">Unit Type</span>: {item.unit_type}</p>
+                                                <div className="custom-hstack">
+                                                <p className="data-p me-1"><span className="label">Station</span>: {item.station}</p>
+                                                <p className="data-p"><span className="label">Shift</span>: {item.shift}</p>
+                                                </div>
+                                                <hr />
+                                                {item.unit_status.dispatched ? (<p className="data-p"><span className="label">Dispatched</span>: {getDateFormat(item.unit_status.dispatched.timestamp)}</p>) : <></>}
+                                                {item.unit_status.acknowledged ? (<p className="data-p"><span className="label">Acknowledged</span>: {getDateFormat(item.unit_status.acknowledged.timestamp)}</p>) : <></>}
+                                                {item.unit_status.enroute ? (<p className="data-p"><span className="label">Enroute</span>: {getDateFormat(item.unit_status.enroute.timestamp)}</p>) : <></>}
+                                                {item.unit_status.cleared ? (<p className="data-p"><span className="label">Cleared</span>: {getDateFormat(item.unit_status.cleared.timestamp)}</p>) : <></>}
+                                                {item.unit_status.arrived ? (<p className="data-p"><span className="label">Arrived</span>: {getDateFormat(item.unit_status.arrived.timestamp)}</p>) : <></>}
+                                                {item.unit_status.available ? (<p className="data-p"><span className="label">Available</span>: {getDateFormat(item.unit_status.available.timestamp)}</p>) : <></>}
+                                                
+                                            </CardContent>
+                                        </Card>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    
+                {/* </Box> */}
+        </Modal>
+        </>
+
     )
 }
 
---
+++ src/interfaces.ts
@@ -0,0 +1,83 @@
+interface Address {
+    address_line1: string,
+    city: string,
+    common_place_name: string
+    cross_street1: string,
+    cross_street2: string,
+    first_due: string,
+    geohash: string,
+    latitude: number,
+    longitude: number,
+    name: string,
+    number: string,
+    postal_code: string,
+    prefix_direction: string,
+    response_zone: string,
+    state: string,
+    suffix_direction: string,
+    type: string 
+  }
+  
+  interface Apparatus {
+    car_id: string,
+    geohash: string,
+    personnel: []
+    shift: string,
+    station: string,
+    unit_id: string,
+    unit_status: UnitStatus,
+    unit_type: string
+  }
+  
+  interface Description {
+    comments: string,
+    day_of_week: string,
+    event_closed: string,
+    event_id: string,
+    event_opened: string,
+    extended_data: Object,
+    first_unit_arrived: string,
+    first_unit_dispatched: string,
+    first_unit_enroute: string,
+    hour_of_day: number,
+    incident_number: string,
+    loi_search_complete: string,
+    subtype: string,
+    type: string
+  }
+  
+  interface FireDepartment{
+    fd_id: string,
+    firecares_id: string,
+    name: string,
+    shift: string,
+    state: string,
+    timezone: string
+  }
+  
+  interface IncidentJson {
+    address: Address,
+    apparatus: Apparatus[],
+    description: Description,
+    fire_department: FireDepartment,
+    version: string
+  }
+
+  interface UnitStatus {
+    acknowledged: StatusItems,
+    arrived: StatusItems,
+    available: StatusItems,
+    cleared: StatusItems,
+    dispatched: StatusItems,
+    enroute: StatusItems,
+
+  }
+
+  interface StatusItems {
+    geohash: string,
+    latitude: number,
+    longitude: number,
+    timestamp: string
+  }
+  
+  export type {Address, Apparatus, Description, FireDepartment, IncidentJson, UnitStatus}
